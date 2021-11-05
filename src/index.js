00b23917d1dd645388f059a34746c264794588af
= <=>
2021-11-04 20:37:49 -0600

    Modified index file

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> d7ab597... Initial commit
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
